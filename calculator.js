15b33cb2849a393cf4cd59f481a1e21b365a872a
MustafovaSemra <mustafovasemra@yahoo.com>
2021-06-03 20:56:03 -0400

    calculator

--- calculator.js
+++ calculator.js
@@ -1,13 +1,33 @@
 function sum(a, b) {
-  return;
+
+  if (a===undefined && b===undefined){
+    return 0
+  } else if (a && b===undefined){
+    return a+0
+  }else {
+    return a+b
+  }
+  
+
 }
 
 function subtract(a, b) {
-  return;
+  if (a===undefined && b===undefined){
+    return 0
+  } else if (a && b===undefined){
+    return a-0
+  }else {
+    return a-b
+  }
 }
 
 function divide(a, b) {
-  return;
+  if (a>0 && b>0){
+    return a/b
+  }
+  if (a>0 && b===0){
+    throw new Error ("Can`t divide by 0!")
+  }
 }
 
 function multiply(a, b) {
